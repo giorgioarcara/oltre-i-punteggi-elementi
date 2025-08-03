00a22c8b6f085353c3c3e80079b72a9caf37c5ff
giorgio.arcara <giorgio.arcara@gmail.com>
2025-08-03 13:31:32 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# Guida alla Psicometria e Statistica per la Neuropsicologia Clinica e Forense
+
+Questa repository include i file per lo sviluppo una breve guida per la Psicometria e Statistica per la Neuropsicologia Clinica e Forense.
+
+Il libro è scritto usando Quarto ed il template di Bruno Rodrigues https://b-rodrigues.github.io/quarto_book_nix/
+
+Giorgio Arcara 2025
+
+
+
+
+
---
+++ define_env.R
@@ -0,0 +1,21 @@
+library(rix)
+
+rix(r_ver = "4.3.1",
+    r_pkgs = c("quarto"),
+    system_pkgs = "quarto",
+    tex_pkgs = c(
+      "amsmath",
+      "framed",
+      "fvextra",
+      "environ",
+      "fontawesome5",
+      "orcidlink",
+      "pdfcol",
+      "tcolorbox",
+      "tikzfill"
+    ),
+    ide = "other",
+    shell_hook = "",
+    project_path = ".",
+    overwrite = TRUE,
+    print = TRUE)
