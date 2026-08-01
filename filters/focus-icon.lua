b4b6d4a8213a8752eefaaf655cb9f14c36bdf8b0
giorgio.arcara <giorgio.arcara@gmail.com>
2026-08-01 16:33:13 +0200

    fixed bugs in cross platform rendering

--- filters/focus-icon.lua
+++ filters/focus-icon.lua
@@ -1,15 +1,27 @@
+local ICON_HEIGHT = "1.5em"
+local ICON_RAISE = "-0.5em"  -- negativo = abbassa, positivo = alza (LaTeX e HTML)
+
 function Header(el)
   if el.level == 2 then
     local text = pandoc.utils.stringify(el.content)
     if text:match("^Focus:") then
       local offset = quarto.project.offset or "."
       local icon_path = offset .. "/Figures/target.png"
-      local icon = pandoc.Image({}, icon_path, "", {
-        height = "1.5em",
-        style = "vertical-align:-0.25em; margin-right:0.35em;"
-      })
-      el.content:insert(1, pandoc.Space())
-      el.content:insert(1, icon)
+
+      if FORMAT:match("latex") then
+        local icon = pandoc.RawInline("latex",
+          "\\raisebox{" .. ICON_RAISE .. "}{\\includegraphics[height=" .. ICON_HEIGHT .. "]{" .. icon_path .. "}}")
+        el.content:insert(1, pandoc.Space())
+        el.content:insert(1, icon)
+      else
+        local valign = FORMAT:match("epub") and "middle" or ICON_RAISE
+        local icon = pandoc.Image({}, icon_path, "", {
+          height = ICON_HEIGHT,
+          style = "vertical-align:" .. valign .. "; margin-right:0.35em;"
+        })
+        el.content:insert(1, pandoc.Space())
+        el.content:insert(1, icon)
+      end
     end
   end
   return el
